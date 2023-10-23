120ef07fdc9f503488cd2e331271db00bd8df733
irumaru <jibakomaorochi13@outlook.jp>
2023-10-23 13:03:42 +0000

    Add content

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,15 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		textColor: theme => theme('color'),
+		textColor: {
+			'primary': '#fffcfc'
+		},
+		backgroundColor: theme => ({
+			...theme('colors'),
+			'primary': '#060620',
+			'secondary': '#171717'
+		})
 	},
 	plugins: [],
 }
