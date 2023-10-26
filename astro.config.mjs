c893cea23242f07ffd9f70762ed089f9af9e6ba2
irumaru <jibakomaorochi13@outlook.jp>
2023-10-26 16:06:06 +0000

    buildホストから環境変数を取得

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+// buildホストから環境変数を取得
+const BASE = process.env.BASE
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://cit-nclab.github.io',
-  base: '/mo12-hp',
+  base: BASE,
 });
