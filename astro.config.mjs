c68988974327663e8643b6ee8bfff83dfceb2636
irumaru <jibakomaorochi13@outlook.jp>
2023-10-30 02:43:01 +0000

    ogpの追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwind from "@astrojs/tailwind";
 
 // buildホストから環境変数を取得
 const BASE = process.env.BASE;
+const SITE = process.env.SITE ? process.env.SITE : "http://127.0.0.1:4321/";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: SITE,
   base: BASE,
 });
